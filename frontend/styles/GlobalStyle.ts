81d760a7de2e96cc578d68417c72d897ca433de5
Pyohogeun <pyohogeun@gmail.com>
2025-06-11 11:39:44 +0900

    feat : styled component 글로벌 적용

---
+++ frontend/styles/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background: #f9f9f9;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default GlobalStyle;
