9c68962bf998551003fed0461263dd059c9a7384
Pyohogeun <pyohogeun@gmail.com>
2026-01-14 16:11:46 +0900

    fix : next outpue : export X

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  // output: "export",
   compiler: {
     // styled-components SSR 지원 활성화
     styledComponents: true,
