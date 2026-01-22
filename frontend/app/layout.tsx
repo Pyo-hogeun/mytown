7c98888ae62f24364f51121f184a04533c218838
Pyohogeun <pyohogeun@gmail.com>
2026-01-22 15:45:06 +0900

    fix : 아이폰 상단 노치 가려짐 이슈

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,16 +4,16 @@ import GlobalStyle from '@/styles/GlobalStyle';
 import { AppProviders } from './providers';
 import Nav from './component/Nav';
 import Script from 'next/script';
-import styled from 'styled-components';
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover", // ✅ 핵심
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
         {/* ✅ 카카오 주소검색 API 추가 */}
         <Script
           src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
@@ -22,19 +22,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body>
         <AppProviders>
-          <AppWrapper>
-            <GlobalStyle />
-            <Nav />
+          <GlobalStyle />
+          <Nav />
 
-            {children}
-          </AppWrapper>
+          {children}
         </AppProviders>
       </body>
     </html>
   );
 }
-const AppWrapper = styled.div`
-  padding-top: env(safe-area-inset-top);
-  padding-bottom: env(safe-area-inset-bottom);
-  // min-height: 100dvh;
-`
--- frontend/styles/GlobalStyle.ts
+++ frontend/styles/GlobalStyle.ts
@@ -6,6 +6,8 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     font-family: sans-serif;
     background: #f9f9f9;
+    padding-top: env(safe-area-inset-top);
+    padding-bottom: env(safe-area-inset-bottom);
   }
 
   a {
