22432d40857c993668b11861e9913df692406076
Pyohogeun <pyohogeun@gmail.com>
2026-01-22 17:32:53 +0900

    fix : 아이폰 상단 노치 가려짐 이슈

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,16 +4,16 @@ import GlobalStyle from '@/styles/GlobalStyle';
 import { AppProviders } from './providers';
 import Nav from './component/Nav';
 import Script from 'next/script';
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-  viewportFit: "cover", // ✅ 핵심
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* ✅ safe-area를 위한 핵심 */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
         {/* ✅ 카카오 주소검색 API 추가 */}
         <Script
           src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
