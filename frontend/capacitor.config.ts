b6804a1ca9ec7ce6688b852bbc031fa22755d59d
Pyohogeun <pyohogeun@gmail.com>
2026-01-26 15:11:37 +0900

    fix : TEST webview

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,10 +3,15 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.mytown.app",
   appName: "MyTown",
-  webDir: "public",
+  webDir: "out",
   server: {
     url: "https://mytown-ruby.vercel.app",
     cleartext: false,
+    // ✅ 외부 도메인 네비게이션/접근 허용(필수로 넣어보는 걸 추천)
+    allowNavigation: [
+      'mytown-ruby.vercel.app',
+      'mytown-myui.onrender.com',
+    ],
   },
 };
 
